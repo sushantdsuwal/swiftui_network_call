0bec27fd74ae54d535043be71d335c01f58046c0
Sushant Suwal <sushantdsuwal@gmail.com>
2024-02-26 09:39:37 +0545

    Initial Commit

---
+++ DecodingJsonRestApi/DecodingJsonRestApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodingJsonRestApiApp.swift
+//  DecodingJsonRestApi
+//
+//  Created by Sushant Suwal on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DecodingJsonRestApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
